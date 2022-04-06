960974c6998836179a59014ecc269e6efe3829dc
jacobkee <jacobkee@nipsea.com.sg>
2022-04-06 16:53:49 +0800

    first

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-
-require("dotenv").config();
 
 const app = express();
 const port = 8888;
 
-app.use(cors());
-app.use(express.json());
-
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const connection = mongoose.connection;
-connection.once("open", () => {
-    console.log("MongoDB database connection established successfully");
-});
-
 app.get("/", (req, res) => res.send("Hello World"));
 
-const networkRouter = require("./routes/network");
-const tokenRouter = require("./routes/token");
-const web3Router = require("./routes/web3");
-
-app.use("/network", networkRouter);
-app.use("/token", tokenRouter);
-app.use("/web3", web3Router);
-
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: http://localhost:${port}`);
 });
