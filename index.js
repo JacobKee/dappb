91b24327bf73fbe4c51d37f56f4d5737d8d3eb7d
jacobkee <jacobkee@nipsea.com.sg>
2022-04-06 17:18:08 +0800

    done

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ connection.once("open", () => {
 
 app.get("/", (req, res) => res.send("Hello World"));
 
-const networkRouter = require("./routes/network");
-const tokenRouter = require("./routes/token");
-const web3Router = require("./routes/web3");
+import networkRouter from "./routes/network";
+import tokenRouter from "./routes/token";
+import web3Router from "./routes/web3";
 
 app.use("/network", networkRouter);
 app.use("/token", tokenRouter);
