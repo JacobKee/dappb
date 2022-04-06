62b2708b82a8ccb06ae61838ad9d2a573f174d11
jacobkee <jacobkee@nipsea.com.sg>
2022-04-06 16:57:41 +0800

    first

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = 8888;
+const port = process.env.PORT || 8888;
 
 app.get("/", (req, res) => res.send("Hello World"));
 
